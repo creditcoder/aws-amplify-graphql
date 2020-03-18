7efab433b06535a17159d294d697b5e87a140420
creditcoder <creditcoder@hotmail.com>
2020-03-19 02:10:05 +0500

    add aws setting

---
+++ src/GraphQL/subscriptions.ts
@@ -0,0 +1,51 @@
+// tslint:disable
+// this is an auto generated file. This will be overwritten
+
+export const onCreatePicture = /* GraphQL */ `
+  subscription OnCreatePicture($owner: String!) {
+    onCreatePicture(owner: $owner) {
+      id
+      name
+      owner
+      visibility
+      file {
+        bucket
+        region
+        key
+      }
+      createdAt
+    }
+  }
+`;
+export const onUpdatePicture = /* GraphQL */ `
+  subscription OnUpdatePicture($owner: String!) {
+    onUpdatePicture(owner: $owner) {
+      id
+      name
+      owner
+      visibility
+      file {
+        bucket
+        region
+        key
+      }
+      createdAt
+    }
+  }
+`;
+export const onDeletePicture = /* GraphQL */ `
+  subscription OnDeletePicture($owner: String!) {
+    onDeletePicture(owner: $owner) {
+      id
+      name
+      owner
+      visibility
+      file {
+        bucket
+        region
+        key
+      }
+      createdAt
+    }
+  }
+`;
